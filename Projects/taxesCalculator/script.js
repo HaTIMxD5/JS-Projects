e36eb1490b0ec6c39e09c87073f71a09dc307584
Hatim <hahatemu@gmail.com>
2025-05-16 15:23:33 +0100

    taxsCalculator

---
+++ Projects/taxesCalculator/script.js
@@ -0,0 +1,13 @@
+let ipt = document.getElementById("ipt");
+let perIpt = document.getElementById("perIpt");
+let btn = document.getElementById("btn");
+let result = document.getElementById("result");
+
+function calculateTax() {
+  let amount = Number(ipt.value);
+  let per = Number(perIpt.value);
+  let total = (amount * (per / 100)) + amount;
+  
+  result.innerHTML = `Your Total Price Is : ${total}$`;
+  result.style.display = "block";
+}
